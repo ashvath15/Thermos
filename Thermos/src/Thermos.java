ce5199bfed408b4318e4a36e521e99009eb712ae
Ashvath <Ashvath@Ashvath>
2022-06-12 21:34:24 +0530

    comm

---
+++ Thermos/src/Thermos.java
@@ -0,0 +1,31 @@
+public class Thermos {
+	String brand;
+	String liquid;
+	int liquidVol; 
+	Thermos(String brand,String liquid){
+		this.liquidVol=0;
+		this.brand=brand;
+		this.liquid=liquid;
+	}
+	
+	void addliquid(int liquid_amt) {
+		liquidVol= liquidVol+ liquid_amt;
+	}
+	
+	void display() {
+		//code
+	}
+	
+	void pourliquid(int liquid_amt) {
+	//code
+		liquidVol= liquidVol- liquid_amt;
+	}
+	
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//
+	}
+	
+}
